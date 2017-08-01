5354b1474bb25f814d00d239ece2af48e8dfe289
redashu <asashuthegreat4@gmail.com>
2017-08-01 08:58:00 +0530

    after long time

--- abc.py
+++ abc.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python
 
-import  os,subprocess
+import  os,subprocess,time
 for  i in  range(20):
 
 	print  "Hello world"
 
+
+time.sleep(3)
+
+def  motd():
+	print  "new data found on my code"
+	print  os.system('sl')
+
+	print  "really work hard"
+
+
+motd()
 
 
